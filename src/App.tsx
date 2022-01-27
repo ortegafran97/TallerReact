a0085918f3f2f81330d460995e178fb76c08a9e0
ortegafran97 <ortegafran97@gmail.com>
2022-01-26 21:06:40 -0300

    Loading changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Counter } from "./features/counter/Counter";
 import { Contactos } from "./Components/Contactos/Index";
 import "./App.css";
 import Header from "./Components/Util/Header";
 import { Outlet } from "react-router-dom";
+import {
+  getVehiculosAsync,
+  selectVehiculos,
+} from "./features/slices/vehiculosSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import {
+  getContactosAsync,
+  selectContactos,
+} from "./features/contactos/contactosSlice";
 // import { Modal } from "react-bootstrap";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const vehiculos = useAppSelector(selectVehiculos);
+  const contactos = useAppSelector(selectContactos);
+
+  useEffect(() => {
+    dispatch(getVehiculosAsync({ vehiculos }));
+    dispatch(getContactosAsync(contactos));
+  }, []);
 
   return (
     <div className="App">
-        <Header />
-        <Outlet />
+      <Header />
+      <Outlet />
     </div>
   );
 }
